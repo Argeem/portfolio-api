2479b32f502b4c749d7e32e46b84d17fc5a39ab7
Argeem <sitthisakbannob@gmail.com>
2025-09-02 22:26:22 +0700

    config nest js log and general setting lint prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configurationConfig from './config/configuration.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurationConfig],
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createLogger, format } from 'winston';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const instanceLog = createLogger({
+    level: 'debug',
+    format: format.combine(
+      format.timestamp(),
+      format.json(),
+      format.prettyPrint(),
+    ),
+  });
+
+  const app = await NestFactory.create(AppModule, {
+    logger: instanceLog,
+  });
+  const configuration = app.get(ConfigService);
+  await app.listen(configuration.get<number>('app.port') || 3000);
 }
 bootstrap();
